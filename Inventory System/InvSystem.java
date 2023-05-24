1db58e79d8efd17b07ada89813f4b9cab8f1fb46
dux <anonymouslydangerous@protonmail.com>
2023-05-24 15:17:49 +0800

    Yas

---
+++ Inventory System/InvSystem.java
@@ -0,0 +1,29 @@
+/* ==== LIBRARY IMPORTS ==== */
+import java.util.*;
+import static java.lang.System.*;
+
+/* ==== MAIN CLASS ==== */
+public class InvSystem {
+
+	// SCANNER //
+	Scanner usrInput = new Scanner(in);
+
+	// GLOBAL VARIABLES //
+	
+
+
+
+
+
+
+
+
+
+
+	// CLEAR CONSOLE TERMINAL //
+	private void clear() {
+		out.print("\033[H\033[2J");
+		out.flush();
+	}
+
+}
